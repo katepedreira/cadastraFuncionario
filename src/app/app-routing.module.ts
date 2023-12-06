c201c43c4eb507b07f4e25edc4dedf8b26109ee5
Katherine Sousa Pedreira <kakamarpe@gmail.com>
2023-12-05 22:55:19 -0300

    merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { IncluirFuncionarioComponent } from './incluir-funcionario/incluir-funcionario.component';
-import { EditarFuncionarioComponent } from './editar-funcionario/editar-funcionario.component';
 import { ListarFuncionariosComponent } from './listar-funcionarios/listar-funcionarios.component';
+import { EditarFuncinarioComponent } from './editar-funcionario/editar-funcinario.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "home", component: HomeComponent},
   {path: "incluir-funcionario", component: IncluirFuncionarioComponent},
-  {path: "editar-funcionario", component: EditarFuncionarioComponent}
+  {path: "editar-funcionario", component: EditarFuncinarioComponent},
   {path: "listar-funcionarios", component: ListarFuncionariosComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
-import { VisualizarFuncionarioComponent } from './visualizar-funcionario/visualizar-funcionario.component';
-import { IncluirFuncionarioComponent } from './incluir-funcionario/incluir-funcionario.component';
-import { EditarFuncionarioComponent } from './editar-funcinario/editar-funcinario.component';
-import { ListarFuncionariosComponent } from './listar-funcionario/listar-funcionarios.component';
-import { CadastrarFuncionarioComponent } from './cadastrar-funcionario/cadastrar-funcionario.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import {MatInputModule } from '@angular/material/input';
-import {FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatSortModule } from '@angular/material/sort';
-import { MatSelectModule } from '@angular/material/select';
-import {HttpClientModule } from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {CadastrarFuncionarioComponent} from './cadastrar-funcionario/cadastrar-funcionario.component';
+import {EditarFuncinarioComponent} from './editar-funcionario/editar-funcinario.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HomeComponent} from './home/home.component';
+import {IncluirFuncionarioComponent} from './incluir-funcionario/incluir-funcionario.component';
+import {ListarFuncionariosComponent} from './listar-funcionarios/listar-funcionarios.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatSortModule} from '@angular/material/sort';
+import {MatSelectModule} from '@angular/material/select';
+import {HttpClientModule} from '@angular/common/http';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
@@ -37,7 +33,7 @@ import { MatDialogModule } from '@angular/material/dialog';
     IncluirFuncionarioComponent,
     CadastrarFuncionarioComponent,
     ListarFuncionariosComponent,
-    EditarFuncionarioComponent,
+    EditarFuncinarioComponent,
     NavbarComponent
   ],
   imports: [
@@ -55,6 +51,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     FormsModule,
     MatSelectModule,
     MatSortModule,
+    MatDialogModule,
+    FormsModule,
     ReactiveFormsModule
   ],
   providers: [],
